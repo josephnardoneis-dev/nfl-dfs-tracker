1a90efa30c5468c811e7e938a053bc3f1a26b1e3
josephnardoneis-dev <josephnardoneis@gmail.com>
2025-09-09 12:06:31 -0400

    Delete build.sh

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Build script for Render deployment
-
-echo "Installing Python packages..."
-pip install --upgrade pip
-pip install -r requirements.txt
-
-echo "Build completed successfully!"
